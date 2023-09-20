87521aa2c62700f047f72cd2930d7618f979d416
Mettralla <67493167+Mettralla@users.noreply.github.com>
2023-09-20 20:06:34 -0300

    agrego channels

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Esto se ejecuta al cargar la pagina
 window.addEventListener('load', function () {
     getProfileName();
     load_user_servers();
@@ -103,6 +104,13 @@ function load_user_servers() {
                     const serverNameP = document.createElement("p");
                     serverNameP.classList.add("svr-name");
                     serverNameP.textContent = server.server_name;
+
+                    // Agrega un evento de clic al div
+                    serverDiv.addEventListener('click', function () {
+                        const server_id = server.server_id;
+                        // Llama a otra función y pasa el server_id
+                        get_channels(server_id);
+                    });
                     
                     uServerDiv.appendChild(IconImg);
                     serverDiv.appendChild(uServerDiv);
@@ -123,3 +131,178 @@ function load_user_servers() {
         document.getElementById("message").innerHTML = "Ocurrio un error";
     });
 }}
+
+function get_channels(server_id) {
+    const channelListDiv = document.createElement("div");
+    channelListDiv.id = "channel-list";
+
+    const addChannelDiv = document.createElement("div");
+    addChannelDiv.id = "add-channel";
+
+    const addButton = document.createElement("button");
+    addButton.id = "btn-new-channel";
+    addButton.textContent = "Agregar Canal";
+
+    addButton.addEventListener('click', function () {
+        newChannelForm(server_id);
+    });
+
+    addChannelDiv.appendChild(addButton);
+
+    document.body.appendChild(channelListDiv);
+    document.body.appendChild(addChannelDiv);
+
+    get_channels_in_server(server_id)
+}
+
+function get_channels_in_server(server_id) {
+    const url = `http://127.0.0.1:5000/tchannels?server_id=${server_id}`;
+
+    fetch(url, {
+        method: 'GET',
+        credentials: 'include'
+    })
+    .then(response => {
+        if (response.status === 200) {
+            return response.json().then(data => {
+                const channelListDiv = document.getElementById("channel-list");
+                channelListDiv.innerHTML = ""; 
+
+                data.channels.forEach(channel => {
+                    const channelDiv = document.createElement("div");
+                    channelDiv.classList.add("channel");
+
+                    const channelNameP = document.createElement("p");
+                    channelNameP.classList.add("channel_name");
+                    channelNameP.textContent = `# ${channel.channel_name}`;
+
+                    channelDiv.addEventListener('click', function () {
+                        alert(`Canal seleccionado: ${channel.channel_name}`);
+                    });
+
+                    channelDiv.appendChild(channelNameP);
+                    channelListDiv.appendChild(channelDiv);
+                })
+        })
+        } else {
+            return response.json().then(data => {
+                alert("Ocurrio un error")
+            });
+        }
+    })
+    .catch(error => {
+        document.getElementById("message").innerHTML = "Ocurrió un error.";
+    });
+}
+
+function newChannelForm(server_id) {
+    // Crear el div principal con clase "create-channel-screen"
+    const createChannelScreenDiv = document.createElement("div");
+    createChannelScreenDiv.classList.add("create-channel-screen");
+
+    // Crear el div con clase "container"
+    const containerDiv = document.createElement("div");
+    containerDiv.classList.add("container");
+
+    // Crear el título h1 con id "title"
+    const titleH1 = document.createElement("h1");
+    titleH1.id = "title";
+    titleH1.textContent = "Nuevo Canal";
+
+    // Crear el formulario con id "authForm"
+    const form = document.createElement("form");
+    form.id = "authForm";
+
+    // Crear el div con clase "form-field"
+    const formFieldDiv = document.createElement("div");
+    formFieldDiv.classList.add("form-field");
+
+    // Crear la etiqueta "label"
+    const label = document.createElement("label");
+    label.setAttribute("for", "channel_name");
+    label.textContent = "Nombre:";
+
+    // Crear el input con clase "input-box" y los atributos necesarios
+    const input = document.createElement("input");
+    input.classList.add("input-box");
+    input.setAttribute("type", "text");
+    input.setAttribute("name", "channel_name");
+    input.setAttribute("id", "channel_name");
+    input.setAttribute("required", true);
+
+    // Crear el botón "Crear" con tipo "submit" y id "auth-btn"
+    const submitButton = document.createElement("button");
+    submitButton.setAttribute("type", "submit");
+    submitButton.id = "auth-btn";
+    submitButton.textContent = "Crear";
+
+    // Crear el botón "Cancelar" con id "cancel-btn"
+    const cancelButton = document.createElement("button");
+    cancelButton.id = "cancel-btn";
+    cancelButton.textContent = "Cancelar";
+
+    // Agregar eventos de clic a los botones
+    submitButton.addEventListener("click", onCrearButtonClick);
+    cancelButton.addEventListener("click", onCancelarButtonClick);
+
+    // Función que se ejecuta cuando se hace clic en el botón "Crear"
+    function onCrearButtonClick(event) {
+        event.preventDefault(); // Evita que el formulario se envíe si está dentro de un formulario
+        // Agrega aquí el código que deseas ejecutar cuando se hace clic en "Crear"
+        createChannel(server_id)
+    }
+
+    // Función que se ejecuta cuando se hace clic en el botón "Cancelar"
+    function onCancelarButtonClick() {
+        // Agrega aquí el código que deseas ejecutar cuando se hace clic en "Cancelar"
+        window.location.href = "../index.html";
+    }
+
+    // Crear el div con id "message"
+    const messageDiv = document.createElement("div");
+    messageDiv.id = "message";
+
+    // Agregar los elementos al formulario y al div principal
+    formFieldDiv.appendChild(label);
+    formFieldDiv.appendChild(input);
+    form.appendChild(formFieldDiv);
+    form.appendChild(submitButton);
+    form.appendChild(cancelButton);
+    containerDiv.appendChild(titleH1);
+    containerDiv.appendChild(form);
+    containerDiv.appendChild(messageDiv);
+    createChannelScreenDiv.appendChild(containerDiv);
+
+    // Insertar el div principal en el body
+    document.body.appendChild(createChannelScreenDiv);
+}
+
+function createChannel(server_id) {
+    const data = {
+        channel_name: document.getElementById("channel_name").value,
+        server_id: server_id,
+    }
+    fetch("http://127.0.0.1:5000/tchannels", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+        credentials: 'include'
+    })
+    .then(response => {
+        if (response.status === 201) {
+            return response.json().then(data => {
+                alert(data.message);
+                window.location.href = "../index.html";
+            });
+        } else {
+            return response.json().then(data => {
+                alert(data.error.description);
+            })
+        }
+    })
+    .catch(error => {
+        alert("Ocurrio un error");
+    });
+}
